2ca4586eaa26f03ee9e1ff70903ca55355584843
OwenFeline <97157122+OwenFeline@users.noreply.github.com>
2022-01-05 19:29:57 +0800

    Update zhTW.lua

--- Localization/zhTW.lua
+++ Localization/zhTW.lua
@@ -39,8 +39,8 @@ L["Quest"] = "任務"
 L["Campaign"] = "戰役"
 L["World Event"] = "世界事件"
 L["World Quest"] = "世界任務"
-L["NPC"] = true
-L["Region"] = "地區"
+L["NPC"] = "目標"
+L["Region"] = "區域"
 L["Cost"] = "費用"
 L["Faction"] = "陣營"
 L["Profession"] = "專業技能"
@@ -52,203 +52,203 @@ L["Spell"] = "法術"
 L["Vendor"] = "商人"
 
 -- Zone Names
-L["Shadowlands"] = true
-L["Ardenweald"] = true
-L["Bastion"] = true
-L["Revendreth"] = true
-L["Maldraxxus"] = true
-L["Oribos"] = true
-L["The Maw"] = true
-L["Torghast"] = true
-L["The Adamant Vaults"] = true
-L["Korthia"] = true
-L["De Other Side"] = true
-L["Mists of Tirna Scithe"] = true
-L["Tazavesh, the Veiled Market"] = true
-L["Castle Nathria"] = true
-L["Sanctum of Domination"] = true
+L["Shadowlands"] = "暗影之境"
+L["Ardenweald"] = "亞登曠野"
+L["Bastion"] = "昇靈堡"
+L["Revendreth"] = "瑞文崔斯"
+L["Maldraxxus"] = "瑪卓薩斯"
+L["Oribos"] = "奧睿博斯"
+L["The Maw"] = "淵喉"
+L["Torghast"] = "托迦司"
+L["The Adamant Vaults"] = "不滅寶庫"
+L["Korthia"] = "科西亞"
+L["De Other Side"] = "彼界境地"
+L["Mists of Tirna Scithe"] = "特那希迷霧"
+L["Tazavesh, the Veiled Market"] = "塔札維許，帷幕市集"
+L["Castle Nathria"] = "納薩亞城"
+L["Sanctum of Domination"] = "統御聖所"
 
 -- Quest and Campaign Names (could be translated automatically through the API?)
-L["Drust and Ashes"] = true
-L["Mending a Broken Hart"] = true
-L["The Power of Elune"] = true
-L["Night Warrior's Curse"] = true
-L["The Power of Night"] = true
+L["Drust and Ashes"] = "佐司特的惡意"
+L["Mending a Broken Hart"] = "治療詛咒"
+L["The Power of Elune"] = "伊露恩之力"
+L["Night Warrior's Curse"] = "黑夜戰士的詛咒"
+L["The Power of Night"] = "黑夜之力"
 
 -- Item Names (could be translated automatically through the API?)
-L["Bounty of the Grove Wardens"] = true
-L["Queen's Conservatory Cache"] = true
-L["War Chest of the Wild Hunt"] = true
-L["Wild Hunt Supplies"] = true
-L["Synvir Lockbox"] = true
-L["Stygian Lockbox"] = true
-L["Riftbound Cache"] = true
-L["Wildseed Root Grain"] = true
-L["Repaired Riftkey"] = true
+L["Bounty of the Grove Wardens"] = "林地看守者的獎賞"
+L["Queen's Conservatory Cache"] = "女王栽培園的寶箱"
+L["War Chest of the Wild Hunt"] = "曠野獵者戰爭寶箱"
+L["Wild Hunt Supplies"] = "曠野獵者補給品"
+L["Synvir Lockbox"] = "鋅維爾帶鎖箱"
+L["Stygian Lockbox"] = "冥魄帶鎖箱"
+L["Riftbound Cache"] = "隙縛寶箱"
+L["Wildseed Root Grain"] = "野性種子根粒"
+L["Repaired Riftkey"] = "修復的裂隙鑰匙"
 
 -- NPC Names (could be translated automatically through the API?)
-L["Lost Soul"] = true
-L["Ma'oh"] = true
-L["Sparkle"] = true
-L["Aithlyn"] = true
-L["Liawyn"] = true
-L["The Grand Menagerie"] = true
-L["Master Clerk Salorn"] = true
-L["Lady Sylvanas Windrunner"] = true
-L["Mueh'Zala"] = true
-L["Mistcaller"] = true
-L["Cortinarius"] = true
-L["Sire Denathrius"] = true
-L["Skuld Vit"] = true
-L["So'leah"] = true
-L["Valfir the Unrelenting"] = true
-L["Spindlenose"] = true
-L["Shopkeeper"] = true
-L["Mystic Rainbowhorn"] = true
+L["Lost Soul"] = "失落的靈魂"
+L["Ma'oh"] = "茂歐"
+L["Sparkle"] = "火花"
+L["Aithlyn"] = "艾瑟琳"
+L["Liawyn"] = "莉亞溫"
+L["The Grand Menagerie"] = "大展示廳"
+L["Master Clerk Salorn"] = "首席營業員沙洛恩"
+L["Lady Sylvanas Windrunner"] = "希瓦娜斯‧風行者女士"
+L["Mueh'Zala"] = "繆薩拉"
+L["Mistcaller"] = "喚霧者"
+L["Cortinarius"] = "寇提納留斯"
+L["Sire Denathrius"] = "戴納瑟斯王"
+L["Skuld Vit"] = "斯寇德‧維特"
+L["So'leah"] = "索利亞"
+L["Valfir the Unrelenting"] = "『冷酷』瓦菲爾"
+L["Spindlenose"] = "軸鼻"
+L["Shopkeeper"] = "店員"
+L["Mystic Rainbowhorn"] = "神秘虹角"
 
 -- Others
-L["Burning Crusade Timewalking"] = true
-L["Wrath of the Lich King Timewalking"] = true
-L["Cataclysm Timewalking"] = true
-L["Mists of Pandaria Timewalking"] = true
-L["Warlords of Draenor Timewalking"] = true
-L["Legion Timewalking"] = true
-L["Pet Battle"] = true
-L["Shadowlands World Bosses"] = true
-L["Queen's Conservatory"] = true
-L["Mythic+ dungeons"] = true
-L["Night Fae dailies"] = true
-L["Mushroom Network"] = true
-L["Marasmius"] = true
-L["Paragon reward."] = true
-L["Soulshape"] = true
-L["Pilgrim's Bounty"] = true
+L["Burning Crusade Timewalking"] = "燃燒的遠征時光漫遊"
+L["Wrath of the Lich King Timewalking"] = "巫妖王之怒時光漫遊"
+L["Cataclysm Timewalking"] = "浩劫與重生時光漫遊"
+L["Mists of Pandaria Timewalking"] = "潘達利亞之謎時光漫遊"
+L["Warlords of Draenor Timewalking"] = "德拉諾之霸時光漫遊"
+L["Legion Timewalking"] = "軍臨天下時光漫遊"
+L["Pet Battle"] = "寵物對戰"
+L["Shadowlands World Bosses"] = "暗影之境世界首領"
+L["Queen's Conservatory"] = "女王的栽培園"
+L["Mythic+ dungeons"] = "傳奇鑰石地城"
+L["Night Fae dailies"] = "暗夜妖精每日任務"
+L["Mushroom Network"] = "蘑菇網路"
+L["Marasmius"] = "瑪拉茲莫斯"
+L["Paragon reward."] = "聲望巔峰獎勵"
+L["Soulshape"] = "靈魂幻體"
+L["Pilgrim's Bounty"] = "旅人豐年祭"
 
-L["Normal or Heroic"] = true
-L["Heroic"] = true
-L["Hardmode"] = true
-L["Any"] = true
+L["Normal or Heroic"] = "普通或英雄"
+L["Heroic"] = "英雄"
+L["Hardmode"] = "困難模式"
+L["Any"] = "任意"
 
 -- Database
-L["Alpaca Soul"] = true
-L["Ardenmoth Soul"] = true
+L["Alpaca Soul"] = "羊駝靈魂"
+L["Ardenmoth Soul"] = "亞登蛾靈魂"
 
-L["Boar Soul"] = true
-L["Boar Soul Guide"] = "Can drop from the last boss of any Warlords Draenor Timewalking dungeon."
+L["Boar Soul"] = "野豬靈魂"
+L["Boar Soul Guide"] = "任何德拉諾之霸時光漫遊地城的尾王掉落。"
 
-L["Bunny Soul"] = true
-L["Bunny Soul Guide"] = "Occasional reward from pet battle world quests in the area."
+L["Bunny Soul"] = "兔子靈魂"
+L["Bunny Soul Guide"] = "戰寵世界任務可能出現的獎勵之一。"
 
-L["Cat Soul"] = true
-L["Cat Soul Guide"] = "A cat soul can spawn on top of the trunk of the tallest Ardenwealed trees (Dreamsong Fenn, Glitterfall Basin, Tirna Vaal, Hibernal Hollow or Claw's Edge). Use /soothe on it to get the soulshape."
+L["Cat Soul"] = "貓靈魂"
+L["Cat Soul Guide"] = "出生點在亞登曠野的參天巨木頂端（夢歌沼地、爍瀑盆地、特納瓦勒、冬日窪地、利爪之緣)。對其輸入/soothe 來獲取。"
 
-L["Cat Soul (Well Fed)"] = true
-L["Cat Soul (Well Fed) Guide"] = "Available once you have unlocked the Cat soulshape. First get the Spectral Feed, then head back to Heart of the Forest, Ma'oh is a cat soul sleeping in a basket near the Queen's Conservatory entrance.\n/meow at him (he'll tell you he's hungry) then feed him the Spectral Feed. You'll receive the soulshape immediately (no quest turn-in needed)."
+L["Cat Soul (Well Fed)"] = "貓靈魂（阿嬤餵的）"
+L["Cat Soul (Well Fed) Guide"] = "解鎖貓靈魂後始可取得。首先取得幽靈雞飼料，接著前往森林之心，有隻叫茂歐的靈魂貓睡在通往女王栽培園的傳送點附近的籃子裡。\n對其輸入/meow （牠會告訴你他餓了），然後餵牠靈魂雞飼料。你將立即解鎖外觀選項而無須繳回任務道具。"
 
-L["Chicken Soul"] = true
-L["Chicken Soul Guide"] = "First get the Spectral Feed, then /chicken at the chicken soul and feed it the Spectral Feed."
+L["Chicken Soul"] = "雞靈魂"
+L["Chicken Soul Guide"] = "首先取得靈魂雞飼料，接著對靈魂雞輸入/chicken，之後餵牠靈魂雞飼料。"
 
-L["Cloud Serpent Soul"] = true
-L["Cloud Serpent Soul Guide"] = "Can drop from the last boss of any Mists of Pandaria Timewalking dungeon."
+L["Cloud Serpent Soul"] = "雲蛟靈魂"
+L["Cloud Serpent Soul Guide"] = "任何潘達利亞之謎時光漫遊地城的尾王掉落。"
 
-L["Cobra Soul"] = true
-L["Cobra Soul Guide"] = "Occasional reward from a win in Arena or Rated Battleground."
+L["Cobra Soul"] = "眼鏡蛇靈魂"
+L["Cobra Soul Guide"] = "競技場或積分戰場勝利時機率獲取。"
 
-L["Corgi Soul"] = true
-L["Corgi Soul Guide"] = "Sparkle is a corgi soul walking around the Heart of the Forest. /pet him and you'll receive the soulshape immediately (no quest turn-in needed)"
+L["Corgi Soul"] = "柯基犬靈魂"
+L["Corgi Soul Guide"] = "有隻叫火花的靈魂柯基犬在森林之心走動。對其輸入/pet，你將立刻解鎖外觀選項而無須繳回任務道具。"
 
-L["Crane Soul"] = true
-L["Cricket Soul"] = true
-L["Direhorn Soul"] = true
-L["Eagle Soul"] = true
-L["Equine Soul"] = true
+L["Crane Soul"] = "羽鶴靈魂"
+L["Cricket Soul"] = "蟋蟀靈魂"
+L["Direhorn Soul"] = "恐角龍靈魂"
+L["Eagle Soul"] = "飛鷹靈魂"
+L["Equine Soul"] = "駿馬靈魂"
 
-L["Frog Soul"] = true
-L["Frog Soul Guide"] = "Available from any covenant zone."
+L["Frog Soul"] = "青蛙靈魂"
+L["Frog Soul Guide"] = "可於任何誓盟所在地圖的水域取得。"
 
-L["Goat Soul"] = true
+L["Goat Soul"] = "山羊靈魂"
 
-L["Gryphon Soul"] = true
-L["Gryphon Soul Guide"] = "Occasional reward from a win in Skirmish, Arena, Random Battleground or Rated Battleground."
+L["Gryphon Soul"] = "獅鷲獸靈魂"
+L["Gryphon Soul Guide"] = "亂鬥、競技場、隨機戰場、積分戰場獲勝時機率取得。"
 
-L["Gulper Soul"] = true
-L["Gulper Soul Guide"] = "Obtainable with spirits of any quality, but use only one %s."
+L["Gulper Soul"] = "大嘴蟾靈魂"
+L["Gulper Soul Guide"] = "不羈魂靈 + 一個野性種子根粒%s"
 
-L["Hippo Soul"] = true
-L["Hippo Soul Guide"] = "Reward from Night Fae Assault."
+L["Hippo Soul"] = "河馬靈魂"
+L["Hippo Soul Guide"] = "暗夜妖精進攻戰的獎勵"
 
-L["Hippogryph Soul"] = true
+L["Hippogryph Soul"] = "角鷹獸靈魂"
 
-L["Hyena Soul"] = true
-L["Hyena Soul Guide"] = "Can drop at any key level."
+L["Hyena Soul"] = "土狼靈魂"
+L["Hyena Soul Guide"] = "任何層數的鑰匙均可能掉落。"
 
-L["Jormungar Soul"] = true
-L["Jormungar Soul Guide"] = "Can drop from the last boss of any Wrath of the Lich King Timewalking dungeon."
+L["Jormungar Soul"] = "蟄猛巨蟲靈魂"
+L["Jormungar Soul Guide"] = "任何巫妖王之怒時光漫遊地城的尾王掉落。"
 
-L["Kodo Soul"] = true
-L["Kodo Soul Guide"] = "Can drop from the last boss of any Cataclysm Timewalking dungeon."
+L["Kodo Soul"] = "科多獸靈魂"
+L["Kodo Soul Guide"] = "任何浩劫與重生時光漫遊地城的尾王掉落。"
 
-L["Leonine Soul"] = true
-L["Lupine Soul"] = true
-L["Mammoth Soul"] = true
+L["Leonine Soul"] = "雄獅靈魂"
+L["Lupine Soul"] = "孤狼靈魂"
+L["Mammoth Soul"] = "長毛象靈魂"
 
-L["Moose Soul"] = true
-L["Moose Soul Guide"] = "You must have defeated 5 of the 7 different rare bosses from the amphitheater performances."
+L["Moose Soul"] = "麋鹿靈魂"
+L["Moose Soul Guide"] = "你必須擊敗過星湖露天劇場七種稀有當中的五種。"
 
-L["Otter Soul"] = true
-L["Otter Soul Guide"] = "An otter soul can spawn in the lake under Hero's Rest (60 minutes respawn). /hug it to receive the soulshape quest item."
+L["Otter Soul"] = "水獺靈魂"
+L["Otter Soul Guide"] = "出生點在英雄之陵底下的湖（重生時間一小時）。對其輸入/hug 來獲得任務道具。"
 
-L["Owl Soul"] = true
-L["Owl Soul Guide"] = "Can drop from the last boss of any Legion Timewalking dungeon."
+L["Owl Soul"] = "貓頭鷹靈魂"
+L["Owl Soul Guide"] = "任何軍臨天下時光漫遊地城的尾王掉落。"
 
-L["Owlcat Soul"] = true
-L["Porcupine Soul"] = true
-L["Prairie Dog Soul"] = true
-L["Ram Soul"] = true
-L["Raptor Soul"] = true
+L["Owlcat Soul"] = "梟羽獸靈魂"
+L["Porcupine Soul"] = "豪豬靈魂"
+L["Prairie Dog Soul"] = "草原土撥鼠靈魂"
+L["Ram Soul"] = "大角羊靈魂"
+L["Raptor Soul"] = "迅猛龍靈魂"
 
-L["Rat Soul"] = true
-L["Rat Soul Guide"] = "When he whispers you, answer with /yes or /nod to gain access to his wares."
+L["Rat Soul"] = "老鼠靈魂"
+L["Rat Soul Guide"] = "當他發送悄悄話時，以表情指令/yes或/nod回應他以查看他的商品。"
 
-L["Runestag Soul"] = true
-L["Runestag Soul Guide"] = "Spawns northeast of Ardenweald."
+L["Runestag Soul"] = "符文雄鹿靈魂"
+L["Runestag Soul Guide"] = "出生點在亞登曠野的東北方。"
 
-L["Saurid Soul"] = true
-L["Saurid Soul Guide"] = "Inside a cave. Target the trash pile at the back of the cave and /bow it."
+L["Saurid Soul"] = "羽冠龍靈魂"
+L["Saurid Soul Guide"] = "洞穴裡面，點選洞穴深處的垃圾堆並輸入/bow。"
 
-L["Saurolisk Soul"] = true
-L["Saurolisk Hatchling Soul"] = true
-L["Shadowstalker Soul"] = true
+L["Saurolisk Soul"] = "尖角蜥靈魂"
+L["Saurolisk Hatchling Soul"] = "尖角幼蜥靈魂"
+L["Shadowstalker Soul"] = "巡影者靈魂"
 
-L["Shoveltusk Soul"] = true
-L["Shoveltusk Soul Guide"] = "Occasional reward from a win in PvP Brawl: Comp Stomp."
+L["Shoveltusk Soul"] = "鍬牙靈魂"
+L["Shoveltusk Soul Guide"] = "PvP亂鬥：對抗電腦獲勝時機率取得。"
 
-L["Shrieker Soul"] = true
-L["Snake Soul"] = true
-L["Snapper Soul"] = true
+L["Shrieker Soul"] = "尖嘯者靈魂"
+L["Snake Soul"] = "蛇靈魂"
+L["Snapper Soul"] = "鉗嘴龜靈魂"
 
-L["Spider Soul"] = true
-L["Spider Soul Guide"] = "Go through a Rift Portal using a %s. There are 4 caches spawning every day at random locations."
+L["Spider Soul"] = "蜘蛛靈魂"
+L["Spider Soul Guide"] = "用%s進入裂隙傳送門，裡面每天都有四個寶箱分布於隨機地點。"
 
-L["Sporebat Soul"] = true
-L["Sporebat Soul Guide"] = "Can drop from the last boss of any Burning Crusade Timewalking dungeon."
+L["Sporebat Soul"] = "孢子蝙蝠靈魂"
+L["Sporebat Soul Guide"] = "任何燃燒的遠征時光漫遊地城的尾王掉落。"
 
-L["Stag Soul"] = true
-L["Stag Soul Guide"] = "Can be randomly offered as a reward from any Night Fae daily in Korthia."
+L["Stag Soul"] = "雄鹿靈魂"
+L["Stag Soul Guide"] = "科西亞由暗夜妖精陣營發布的每日任務可能的獎勵之一。"
 
-L["Tiger Soul"] = true
+L["Tiger Soul"] = "猛虎靈魂"
 
-L["Turkey Soul"] = true
-L["Turkey Soul Guide"] = "Go to a faction capital, and find a Pilgrim’s Bounty table.\nEat every food until you have the 5 food buffs, then continue eating food and switching seats every time until you get the soulshape."
+L["Turkey Soul"] = "火雞靈魂"
+L["Turkey Soul Guide"] = "前往陣營主城，找到旅人豐年祭餐桌。\n吃下食物直到你獲得五層食物增益，接著換位置繼續吃直到你獲得靈魂幻體為止。"
 
-L["Ursine Soul"] = true
-L["Veilwing Soul"] = true
+L["Ursine Soul"] = "巨熊靈魂"
+L["Veilwing Soul"] = "幕翼靈魂"
 
-L["Vulpine Soul"] = true
-L["Vulpine Soul Guide"] = "The very first soulshape of your collection!"
+L["Vulpine Soul"] = "狐狸靈魂"
+L["Vulpine Soul Guide"] = "你最初獲得的靈魂！"
 
-L["Wolfhawk Soul"] = true
-L["Wolfhawk Soul Guide"] = "You need to use Soulshape to cross the barrier."
+L["Wolfhawk Soul"] = "狼鷹靈魂"
+L["Wolfhawk Soul Guide"] = "使用靈魂幻體來穿過屏障。"
 
-L["Wyvern Soul"] = true
-L["Yak Soul"] = true
+L["Wyvern Soul"] = "雙足飛龍靈魂"
+L["Yak Soul"] = "氂牛靈魂"
