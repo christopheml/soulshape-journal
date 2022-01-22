36d3f021de1cc6a278139dc4337962715f0536a4
Christophe Michel <christopheml@users.noreply.github.com>
2022-01-22 19:35:07 +0100

    Fixed typo in enUS locale

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -30,7 +30,7 @@ L["TAB_TITLE"] = "Soulshapes"
 L["COUNT_LABEL"] = "Total"
 L["UNTRACKABLE_TOOLTIP_TITLE"] = "This soulshape can't be automatically detected"
 L["UNTRACKABLE_TOOLTIP_CLICK_ME"] = "Click to mark %s as collected if you already have it."
-L["WARNING_NOT_NIGHT_FAE"] = "You're not a member of the Night Fae,\nyou can't collecte new soulshapes at this time."
+L["WARNING_NOT_NIGHT_FAE"] = "You're not a member of the Night Fae,\nyou can't collect new soulshapes at this time."
 
 -- Labels
 L["Loot"] = true
